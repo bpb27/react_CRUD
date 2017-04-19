a9b0b46e87e01f9841b4a123c56fe413ec5ed5c6
brendan brown <brendanbrown27@gmail.com>
2017-04-19 21:05:13 +0200

    more contacts and build

--- src/contacts.js
+++ src/contacts.js
@@ -3,7 +3,11 @@ var list = [
   {name: 'Billy Balderdash', email: 'billy123@hotmail.com', phone: '8185555552'},
   {name: 'Bobby Balloonanimals', email: 'bobs_balloons!@hotmail.com', phone: '(818) 555-5553'},
   {name: 'Burt Basket-Weaver', email: 'burtweaves1999@hotmail.com', phone: '818-555-5554'},
-  {name: 'Bertrand Bobbypins', email: 'bertiepies@hotmail.com', phone: '818-555-5555'}
+  {name: 'Bertrand Bobbypins', email: 'bertiepies@hotmail.com', phone: '818-555-5555'},
+  {name: 'Hank Frankenhank', email: 'frank+blanks@hotmail.com', phone: '323-555-5555'},
+  {name: 'Bertha Chudhammer', email: 'bertha@hotmail.com', phone: '240-555-5555'},
+  {name: 'Clara Clams', email: 'pearls4lyfe@hotmail.com', phone: '626-555-5555'},
+  {name: 'Gary Grapefruits', email: 'orangejuiceh8tr@hotmail.com', phone: '1235555555'},
 ];
 
 export default list
